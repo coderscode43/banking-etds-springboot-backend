82d7fe270003785e1410d075f4f5cc87d4cd7f85
staff10 <staff10@DESKTOP-NF1KPSB.mshome.net>
2019-06-26 17:13:51 +0530

    changes for vendor payment

---
+++ src/main/java/domain/in/rjsa/model/form/VendorPayment.java
@@ -0,0 +1,63 @@
+package domain.in.rjsa.model.form;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "vendorpayment")
+public class VendorPayment extends CommonModelAbstract{
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+	@GenericGenerator(name = "native", strategy = "native")
+	@Column(name = "id")
+	public Long id;		
+	@Column(name = "clientId")
+	public Long clientId;
+	@Column(name = "branchId")
+	public Long branchId;
+	@Column(name = "invoiceDate")
+	public Date invoiceDate;
+	@Column(name = "paymentDone")
+	public String paymentDone;	
+	@Column(name = "paymentDate")
+	public Date paymentDate;
+	@Column(name = "paymentMode")
+	public String paymentMode;
+	@Column(name = "invoiceNumber")
+	public String invoiceNumber;
+	@Column(name = "totalInvoiceValue")
+	public Double totalInvoiceValue;
+	@Column(name = "taxableValue")
+	public Double taxableValue;
+	@Column(name = "cgst")
+	public Double cgst;
+	@Column(name = "sgst")
+	public Double sgst;
+	@Column(name = "igst")
+	public Double igst;
+	@Column(name = "cess")
+	public String Double;
+	@Column(name = "incomeTaxTds")
+	public Double incomeTaxTds;
+	@Column(name = "gstTds")
+	public Double gstTds;
+	@Column(name = "natureOfPayment")
+	public String natureOfPayment;
+	@Column(name = "netAmountPaid")
+	public Double netAmountPaid;
+	@Column(name = "bglCode")
+	public String bglCode;
+	@Column(name = "ourGst")
+	public String ourGst;
+}

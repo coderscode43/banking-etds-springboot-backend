18825209bebdb9ea82dba6913c441b6695f2184e
Rahul <zenithrahul10@gmail.com>
2019-06-29 12:54:32 +0530

    Mis Report changes

---
+++ src/main/java/domain/in/rjsa/controller/MisReportController.java
@@ -0,0 +1,29 @@
+package domain.in.rjsa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import domain.in.rjsa.model.form.MisReport;
+import domain.in.rjsa.service.MisReportService;
+@Controller
+@RequestMapping("/apimisReport")
+public class MisReportController extends AbstractController<Long, MisReport, MisReportService> {
+	@Autowired
+	MisReportService service;
+
+	@Override
+	public MisReportService getService() {
+		// TODO Auto-generated method stub
+		return service;
+	}
+
+	@Override
+	public Class<MisReport> getEntity() {
+		// TODO Auto-generated method stub
+		return MisReport.class;
+	}
+	
+	}
+
+
---
+++ src/main/java/domain/in/rjsa/dao/MisReportDao.java
@@ -0,0 +1,7 @@
+package domain.in.rjsa.dao;
+
+import domain.in.rjsa.model.form.MisReport;
+
+public interface MisReportDao extends DaoInterface<Long,   MisReport> {
+
+}

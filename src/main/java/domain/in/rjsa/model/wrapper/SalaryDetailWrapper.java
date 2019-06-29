47f7ea65be8303c46e4cf1def0cab936d73c8705
staff10 <staff10@DESKTOP-NF1KPSB.mshome.net>
2019-06-29 12:16:53 +0530

    changes for form24Q4salary detail

---
+++ src/main/java/domain/in/rjsa/model/wrapper/SalaryDetailWrapper.java
@@ -0,0 +1,12 @@
+package domain.in.rjsa.model.wrapper;
+
+
+import domain.in.rjsa.model.form.Regular24Q4Salary;
+import lombok.Data;
+@Data
+public class SalaryDetailWrapper {
+	
+	public Regular24Q4Salary salary;
+	
+	
+}

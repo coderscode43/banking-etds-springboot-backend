a74fabe1f4466f19047d1262d1d46441ee672fe7
Rahul <zenithrahul10@gmail.com>
2019-07-02 16:55:25 +0530

    DETAIL REGULAR CHANGES

--- src/main/java/domain/in/rjsa/service/Regular24Q4ChallanService.java
+++ src/main/java/domain/in/rjsa/service/Regular24Q4ChallanService.java
@@ -3,5 +3,5 @@ package domain.in.rjsa.service;
 import domain.in.rjsa.model.form.Regular24Q4Challan;
 
 public interface Regular24Q4ChallanService extends ServiceInterface<Long,Regular24Q4Challan>{
-
+	 public Regular24Q4Challan getByKey(Long id);
 }

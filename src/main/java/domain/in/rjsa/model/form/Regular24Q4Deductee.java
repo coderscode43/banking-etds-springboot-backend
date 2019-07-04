c9267028b7821e982382fdee542acfb4ea24a2cf
Rahul <zenithrahul10@gmail.com>
2019-07-04 18:21:45 +0530

    VALIDATION

--- src/main/java/domain/in/rjsa/model/form/Regular24Q4Deductee.java
+++ src/main/java/domain/in/rjsa/model/form/Regular24Q4Deductee.java
@@ -10,6 +10,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -21,6 +24,10 @@ import lombok.Data;
 @Entity
 @Table(name = "regular24Q4Deductee")
 public class Regular24Q4Deductee extends CommonModelAbstract {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
 	@GenericGenerator(name = "native", strategy = "native")
@@ -31,16 +38,28 @@ public class Regular24Q4Deductee extends CommonModelAbstract {
 	@Column(name = "srNo")
 	public Long srNo;
 	@Column(name = "challanSrNo")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Digits(integer = 12, fraction = 0,message="Enter valid Challan No. Challan No. should not be greater than 12")
 	public Long challanSrNo;
 	@Column(name = "employeeRefNo")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String employeeRefNo;
 	@Column(name = "panRefNo")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String panRefNo;
 	@Column(name = "pan")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String pan;
 	@Column(name = "name")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String name;
 	@Column(name = "sectionCode")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String sectionCode;
 	@Temporal(TemporalType.DATE)
 	@Column(name = "dateOfPayment")
@@ -51,23 +70,43 @@ public class Regular24Q4Deductee extends CommonModelAbstract {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	public Date dateOfDeduction;
 	@Column(name = "amountPaid")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not proper.Maximum 2 fractions allowed.")
 	public Double amountPaid;
 	@Column(name = "tds")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not proper.Maximum 2 fractions allowed.")
 	public Double tds;
 	@Column(name = "surcharge")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not proper.Maximum 2 fractions allowed.")
 	public Double surcharge;
 	@Column(name = "educationCess")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not proper.Maximum 2 fractions allowed.")
 	public Double educationCess;
 	@Column(name = "totalTds")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String totalTds;
 	@Column(name = "totalTaxDeposited")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not proper.Maximum 2 fractions allowed.")
 	public Double totalTaxDeposited;
 	@Column(name = "certificateNumber")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String certificateNumber;
 	@Column(name = "remarks")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String remarks;
 	@Column(name = "fy")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String fy;
 	@Column(name = "quarter")
+	@NotNull(message = "InvoiceDate is a required field.")
+	@Size(min=0, max=45, message="Nature Of Document should not be greater than 45 characters.")
 	public String quarter;
 }

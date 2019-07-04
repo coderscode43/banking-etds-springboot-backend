38bbd8a21eccd440cb19bb63a749f80bcde28926
Rahul <zenithrahul10@gmail.com>
2019-07-04 20:16:27 +0530

    validation changes

--- src/main/java/domain/in/rjsa/model/form/Regular27QChallan.java
+++ src/main/java/domain/in/rjsa/model/form/Regular27QChallan.java
@@ -10,9 +10,12 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -31,54 +34,89 @@ public class Regular27QChallan extends CommonModelAbstract{
 	public Long clientId;
 	@Column(name = "srNo")
 	public Long srNo;
+	
 	@Column(name = "tds")
+	@NotNull(message = "TDS Value is required")
+	@Digits(integer = 12, fraction = 0,message="The amount value is not greater than 12 digits. Maximum 2 fractions allowed.")
 	public Double tds;
+	
 	@Column(name = "surcharge")
+	@NotNull(message = "Surcharge Value is required")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not greater than 12 digits. Maximum 2 fractions allowed.")
 	public Double surcharge;
+	
 	@Column(name = "eduCess")
+	@NotNull(message = "Education Cess Value is required")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not greater than 12 digits. Maximum 2 fractions allowed.")
 	public Double eduCess;
+	
 	@Column(name = "interest")
+	@NotNull(message = "Interest is required")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not greater than 12 digits. Maximum 2 fractions allowed.")
 	public Double interest;
 	
 	@Column(name = "fee")
+	@NotNull(message = "Fee is required")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not greater than 12 digits. Maximum 2 fractions allowed.")
 	public Double fee;
 	
 	@Column(name = "others")
+	@NotNull(message = "Others is required")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not greater than 12 digits. Maximum 2 fractions allowed.")
 	public Double others;
+	
 	@Column(name = "totalTaxDeposit")
+	@NotNull(message = "Total Tax Deposit is required")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not greater than 12 digits. Maximum 2 fractions allowed.")
 	public Double totalTaxDeposit;
 	
 	@Column(name = "tdsDepositBookEntry")
-	@Size(min=0, max=45, message="tdsDepositBookEntry should not be greater than 45 characters.")
+	@NotNull(message = "TDS Deposit Book Entry is required")
+	@Size(min=0, max=45, message="TDS Deposit Book Entry should not be greater than 45 characters.")
 	public String tdsDepositBookEntry;
 	
-	
 	@Column(name = "bankBranchCode")
+	@NotNull(message = "Bank Branch Code is required")
+	@Size(min=0, max=45, message="Bank Branch Code should not be greater than 45 characters.")
 	public String bankBranchCode;
+	
 	@Column(name = "challanSrNo")
+	@NotNull(message = "Challan Sr.No is required")
+	@Digits(integer = 12, fraction = 0,message="The amount value is not proper.")
 	public Long challanSrNo;
+	
 	@Temporal(TemporalType.DATE)
 	@Column(name = "taxDepositDate")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	public Date taxDepositDate;
 	
 	@Column(name = "minorHeadChallan")
+	@NotNull(message = "Minor Head Of Challan is required")
+	@Size(min=0, max=45, message="Minor Head Of Challan should not be greater than 45 characters.")
 	public String minorHeadChallan;
 	
 	@Column(name = "interestAllocated")
+    @NotNull(message = "Interest Allocated is required")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not greater than 12 digits. Maximum 2 fractions allowed.")
 	public Double interestAllocated;
-	@Column(name = "otherAmtAllocated")
+	
+    @Column(name = "otherAmtAllocated")
+    @NotNull(message = "Other Amount Allocated is required")
+	@Digits(integer = 12, fraction = 2,message="The amount value is not greater than 12 digits. Maximum 2 fractions allowed.")
 	public Double otherAmtAllocated;
+	
 	@Column(name = "nilChallanIndicator")
+	@NotNull(message = "Nill Challan Indicator is required")
+	@Size(min=0, max=45, message="Nill Challan Indicator should not be greater than 45 characters.")
 	public String nilChallanIndicator;
-	@Column(name = "warningDescription")
-	public String warningDescription;
-	@Column(name = "shortPayment")
-	public Double shortPayment;
-	@Column(name = "interestShortPay")
-	public Double interestShortPay;
+	
 	@Column(name = "fy")
+    @NotNull(message = "Financial Year is required")
+	@Size(min=0, max=45, message="Financial Year should not be greater than 45 characters.")
 	public String fy;
+	
 	@Column(name = "quarter")
+	@NotNull(message = "Quarter is required")
+	@Size(min=0, max=45, message="Nill Challan Indicator should not be greater than 45 characters.")
 	public String quarter;
 }

ba9924045246422bda0be3007eacac092803fa11
staff10 <staff10@DESKTOP-NF1KPSB.mshome.net>
2019-07-10 15:27:35 +0530

    Changes for request correction for salary details

--- src/main/java/domain/in/rjsa/model/wrapper/RequestCorrectionWrapper.java
+++ src/main/java/domain/in/rjsa/model/wrapper/RequestCorrectionWrapper.java
@@ -1,7 +1,9 @@
 package domain.in.rjsa.model.wrapper;
 
 import java.util.List;
-
+import domain.in.rjsa.model.form.Regular24Q4Challan;
+import domain.in.rjsa.model.form.Regular24Q4Deductee;
+import domain.in.rjsa.model.form.Regular24Q4Salary;
 import domain.in.rjsa.model.form.Regular26QChallan;
 import domain.in.rjsa.model.form.Regular26QDeductee;
 import domain.in.rjsa.model.form.RequestCorrection;
@@ -12,6 +14,9 @@ public class RequestCorrectionWrapper {
 
 	public List<Regular26QDeductee> Regular26QDeducteeList;
 	public List<Regular26QChallan> Regular26QChallanList;
+	public List<Regular24Q4Challan> Regular24Q4ChallanList;
+	public List<Regular24Q4Deductee> Regular24Q4DeducteeList;
+	public List<Regular24Q4Salary> Regular24Q4SalaryList;
 	public RequestCorrection reqCorrection;
 	
 }

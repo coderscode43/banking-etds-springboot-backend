be70746f744aae0e91666f6761050e1fc80f2d4f
Rahul <zenithrahul10@gmail.com>
2019-07-17 12:25:32 +0530

    bank and salary details changes

--- src/main/java/domain/in/rjsa/model/wrapper/EmployeeDetailWrapper.java
+++ src/main/java/domain/in/rjsa/model/wrapper/EmployeeDetailWrapper.java
@@ -1,15 +1,24 @@
 package domain.in.rjsa.model.wrapper;
 
+import java.util.List;
+
 import domain.in.rjsa.model.form.Address;
 import domain.in.rjsa.model.form.BankAccDetail;
 import domain.in.rjsa.model.form.Employee;
+import domain.in.rjsa.model.form.Salary;
 import lombok.Data;
 @Data
 public class EmployeeDetailWrapper {
 	public BankAccDetail bank;
+	//public List <BankAccDetail> bService;
 	public Address paddress;
 	public Address taddress;
 	public Employee employee;
+	public Salary salary;
+	public void setVpList(List<BankAccDetail> vendorPay) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }

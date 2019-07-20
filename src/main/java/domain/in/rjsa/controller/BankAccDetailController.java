267a47c890965994d7eb012c56d266c9cf7836a7
Rahul Soni <zenithrahul10@gmail.com>
2019-07-20 20:26:07 +0530

    changes

--- src/main/java/domain/in/rjsa/controller/BankAccDetailController.java
+++ src/main/java/domain/in/rjsa/controller/BankAccDetailController.java
@@ -1,17 +1,24 @@
 package domain.in.rjsa.controller;
 
+import java.util.LinkedHashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import domain.in.rjsa.model.form.BankAccDetail;
+import domain.in.rjsa.model.form.Login;
+import domain.in.rjsa.model.wrapper.PensionerDetailWrapper;
 import domain.in.rjsa.service.BankAccDetailService;
+import domain.in.rjsa.web.ApplicationCache;
 
 @Controller
 @RequestMapping("/apibank")
 public class BankAccDetailController extends AbstractController<Long, BankAccDetail, BankAccDetailService>{
 	@Autowired
 	BankAccDetailService service;
+	@Autowired
+	ApplicationCache applicationCache;
 		@Override
 		public BankAccDetailService getService() {
 			// TODO Auto-generated method stub
@@ -23,5 +30,6 @@ public class BankAccDetailController extends AbstractController<Long, BankAccDet
 			// TODO Auto-generated method stub
 			return BankAccDetail.class;
 		}
-
+		
+			
 }

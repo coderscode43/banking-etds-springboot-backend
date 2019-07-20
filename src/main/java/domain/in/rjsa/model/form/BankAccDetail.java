8d1d2d060975eff4e8189199dc8eff1a920d6a2a
Rahul <zenithrahul10@gmail.com>
2019-07-20 14:49:15 +0530

    synced

--- src/main/java/domain/in/rjsa/model/form/BankAccDetail.java
+++ src/main/java/domain/in/rjsa/model/form/BankAccDetail.java
@@ -31,21 +31,21 @@ public class BankAccDetail extends CommonModelAbstract {
 	@Column(name = "clientId")
 	private Long clientId;
 	@Column(name = "bankName")
-	@Size(min=0, max=45, message="Bank Name should be 45 characters.")
+	//@Size(min=0, max=45, message="Bank Name should be 45 characters.")
 	private String bankName;
 	@Column(name = "acNo")
-	@Size(min=0, max=45, message="Account No should be 45 characters.")
+	//@Size(min=0, max=45, message="Account No should be 45 characters.")
 	private String acNo;
 	@Column(name = "branch")
-	@Size(min=0, max=45, message="Branch should be 45 characters.")
+	//@Size(min=0, max=45, message="Branch should be 45 characters.")
 	private String branch;
 	@Column(name = "ifsc")
-	@Size(min=0, max=45, message="IFSC Code should be 45 characters.")
+	//@Size(min=0, max=45, message="IFSC Code should be 45 characters.")
 	private String ifsc;
 	@Column(name = "acType")
-	@Size(min=0, max=45, message="Account Type should be 45 characters.")
+//	@Size(min=0, max=45, message="Account Type should be 45 characters.")
 	private String acType;
 	@Column(name = "micrNo")
-	@Size(min=0, max=45, message="MICR No should be 45 characters.")
+	//@Size(min=0, max=45, message="MICR No should be 45 characters.")
 	private String micrNo;
 }

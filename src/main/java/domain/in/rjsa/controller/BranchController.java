b95a157a735b4ace1a617a3ae7afa41df9e69c49
Rahul SOni <zenithrahul10@gmail.com>
2022-03-21 16:22:43 +0530

    CHANGES TO VIEW ONLY PARTICULAR BRANCH

--- src/main/java/domain/in/rjsa/controller/BranchController.java
+++ src/main/java/domain/in/rjsa/controller/BranchController.java
@@ -1,7 +1,5 @@
 package domain.in.rjsa.controller;
 
-
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -49,18 +47,17 @@ import domain.in.rjsa.web.ApplicationCache;
 @Controller
 @RequestMapping("/apibranch")
 public class BranchController {
-	
+
 	@Autowired
 	BranchService service;
-	
+
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	@Autowired
 	ApplicationCache applicationCache;
-	
+
 	@Autowired
 	LogsService lservice;
-	
-	
+
 	/* pranay */
 	@RequestMapping(value = "/ajax", method = RequestMethod.POST)
 	public ResponseEntity<?> ajax(@RequestBody Ajax ajax) {
@@ -74,14 +71,15 @@ public class BranchController {
 		}
 
 	}
+
 	public List<String> getAjax(String name, String term) {
 		// TODO Auto-generated method stub
 		return service.ajax(name, term);
 	}
-	
+
 	@RequestMapping(value = "/search/get/{pageNo}/{resultPerPage}/{json}/**", method = RequestMethod.GET)
-	public ResponseEntity<?> search(@PathVariable String json, HttpServletRequest request,
-			@PathVariable int pageNo, @PathVariable int resultPerPage) {
+	public ResponseEntity<?> search(@PathVariable String json, HttpServletRequest request, @PathVariable int pageNo,
+			@PathVariable int resultPerPage) {
 		try {
 			final String path = request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
 			final String bestMatchingPattern = request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE)
@@ -103,6 +101,9 @@ public class BranchController {
 			map = mapper.readValue(searchParam, new TypeReference<Map<String, String>>() {
 			});
 
+			if (!"admin".equals(applicationCache.getLoginDetail(getPrincipal()).getType())) {
+				map.put("branchCode", applicationCache.getLoginDetail(getPrincipal()).getBranchCode());
+			}
 			Long count = service.findSearchCount(map);
 			List<?> list = getSearch(map, pageNo, resultPerPage);
 //			ListCount send = new ListCount();
@@ -116,25 +117,31 @@ public class BranchController {
 		}
 
 	}
+
 	public List<?> getSearch(LinkedHashMap<?, ?> map, int pageNo, int resultPerPage) {
 		// TODO Auto-generated method stub
 		return service.search(map, pageNo, resultPerPage);
 	}
-	
+
 	// ------------------- Add Entity ---------------------------------
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<?> createEntity(@RequestBody LinkedHashMap<String, Object> entity) {
 		logger.info("Creating new Return instance");
-		
-		service.saveNewUser(entity.get("userName").toString(), entity.get("password").toString());
-		entity.remove("userName");
-		entity.remove("password");
-		create(entity);
-		addLogs(entity);
-		return new ResponseEntity<Object>(HttpStatus.CREATED);
+
+		if ("admin".equals(applicationCache.getLoginDetail(getPrincipal()).getType())) {
+			service.saveNewUser(entity.get("userName").toString(), entity.get("password").toString());
+			entity.remove("userName");
+			entity.remove("password");
+			create(entity);
+			addLogs(entity);
+
+			return new ResponseEntity<Object>(HttpStatus.CREATED);
+		}
+		return new ResponseEntity<Object>(HttpStatus.FORBIDDEN);
 
 	}
+
 	public void create(LinkedHashMap<?, ?> entity) {
 		Gson gson = new Gson();
 		JsonElement jsonElement = gson.toJsonTree(entity);
@@ -146,48 +153,50 @@ public class BranchController {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// ------------------- Get Detail ---------------------------------
-		@RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
-		public ResponseEntity<?> getDetailController(@PathVariable Long id) {
-			// verify the clientId authorization
-			
-			try {
-				return new ResponseEntity<>(getDetail(id), HttpStatus.OK);
-			} catch (Exception e) {
-				logger.error("Error in getting detail ", e);
-				return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-			}
+	@RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getDetailController(@PathVariable Long id) {
+		// verify the clientId authorization
 
+		try {
+			return new ResponseEntity<>(getDetail(id), HttpStatus.OK);
+		} catch (Exception e) {
+			logger.error("Error in getting detail ", e);
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 
-		public Object getDetail(Long id) {
-			// TODO Auto-generated method stub
-			HashMap<String, Object> constrains = new HashMap<>();
-			constrains.put("id", id);
-			return service.uniqueSearch(constrains);
-		}
-		
-		// ------------------- Update Entity ---------------------------------
-		@RequestMapping(value = "/update", method = RequestMethod.PUT)
-		public ResponseEntity<?> update(@RequestBody LinkedHashMap<?, ?> entity, HttpServletResponse response,
-			 UriComponentsBuilder ucBuilder) {
+	}
+
+	public Object getDetail(Long id) {
+		// TODO Auto-generated method stub
+		HashMap<String, Object> constrains = new HashMap<>();
+		constrains.put("id", id);
+		return service.uniqueSearch(constrains);
+	}
+
+	// ------------------- Update Entity ---------------------------------
+	@RequestMapping(value = "/update", method = RequestMethod.PUT)
+	public ResponseEntity<?> update(@RequestBody LinkedHashMap<?, ?> entity, HttpServletResponse response,
+			UriComponentsBuilder ucBuilder) {
+		if ("admin".equals(applicationCache.getLoginDetail(getPrincipal()).getType())) {
 			FieldErrorDTO ermsg = new FieldErrorDTO();
 			Long id = Long.valueOf(entity.get("id").toString());
-			update(entity,id);
+			update(entity, id);
 			ermsg.setMessage("Updated Successfully");
 			return new ResponseEntity<String>(HttpStatus.ACCEPTED);
+		}
+		return new ResponseEntity<Object>(HttpStatus.FORBIDDEN);
+	}
 
-		}
-		public void update(LinkedHashMap<?, ?> entity ,Long id) {
-			Gson gson = new Gson();
-			JsonElement jsonElement = gson.toJsonTree(entity);
-			service.update( gson.fromJson(jsonElement, Branch.class));
-		}
-	
+	public void update(LinkedHashMap<?, ?> entity, Long id) {
+		Gson gson = new Gson();
+		JsonElement jsonElement = gson.toJsonTree(entity);
+		service.update(gson.fromJson(jsonElement, Branch.class));
+	}
+
 	/* END-pranay */
-	
-	
+
 	public String getPrincipal() {
 		String userName = null;
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -200,17 +209,21 @@ public class BranchController {
 		return userName;
 
 	}
+
 	@RequestMapping(value = "/list/count/", method = RequestMethod.GET)
-	public ResponseEntity<?> count( HttpServletRequest request) {
+	public ResponseEntity<?> count(HttpServletRequest request) {
 		// verify the clientId authorization
 //			applicationCache.getUserAuthorised();
 		HashMap<String, Object> constrains = new HashMap<>();
+		if (!"admin".equals(applicationCache.getLoginDetail(getPrincipal()).getType())) {
+			constrains.put("branchCode", applicationCache.getLoginDetail(getPrincipal()).getBranchCode());
+		}
 
 		String mapping = request.getPathInfo();
 
 		try {
 			Long count = service.findallCount(constrains);
-			List<?> list = getList( 0, 100);
+			List<?> list = getList(constrains,0, 100);
 			ListCount send = new ListCount();
 			send.setCount(count);
 			send.setEntities(list);
@@ -221,11 +234,11 @@ public class BranchController {
 		}
 
 	}
-	public List<?> getList( int pageNo, int resultPerPage) {
-		HashMap<String, Object> constrains = new HashMap<>();
+
+	public List<?> getList(HashMap<String, Object> constrains,int pageNo, int resultPerPage) {
 		return service.findAll(constrains, pageNo, resultPerPage);
 	}
-	
+
 	public void addLogs(HashMap<String, Object> entity) {
 
 		Login l = applicationCache.getLoginDetail(getPrincipal());
@@ -239,7 +252,7 @@ public class BranchController {
 		String s = getEntity().getName();
 		String[] arrOfStr = s.split(".", 27);
 		for (String a : arrOfStr)
-			log.setEntity( a );
+			log.setEntity(a);
 		Gson gason = new Gson();
 		String json = gason.toJson(entity);
 		log.setDate(new Date(System.currentTimeMillis()));
@@ -248,10 +261,11 @@ public class BranchController {
 		lservice.save(log);
 
 	}
-	
+
 	public Class<Branch> getEntity() {
 		return Branch.class;
 	}
+
 	private String getIp() {
 		try {
 			InetAddress ipAddr = InetAddress.getLocalHost();
@@ -263,5 +277,5 @@ public class BranchController {
 			return null;
 		}
 	}
-	
+
 }

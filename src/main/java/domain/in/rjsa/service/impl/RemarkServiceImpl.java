830738dacc6844007809f6d1bb01e610fff61e67
Rahul <zenithrahul10@gmail.com>
2022-03-21 18:35:16 +0530

    synced

--- src/main/java/domain/in/rjsa/service/impl/RemarkServiceImpl.java
+++ src/main/java/domain/in/rjsa/service/impl/RemarkServiceImpl.java
@@ -1,7 +1,6 @@
 package domain.in.rjsa.service.impl;
 
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +10,18 @@ import org.springframework.transaction.annotation.Transactional;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
+import domain.in.rjsa.dao.Regular24QDeducteeDao;
+import domain.in.rjsa.dao.Regular26QDeducteeDao;
+import domain.in.rjsa.dao.Regular27EQDeducteeDao;
+import domain.in.rjsa.dao.Regular27QDeducteeDao;
 import domain.in.rjsa.dao.RemarkDao;
+import domain.in.rjsa.dao.TicketDao;
+import domain.in.rjsa.model.fy.Regular24QDeductee;
+import domain.in.rjsa.model.fy.Regular26QDeductee;
+import domain.in.rjsa.model.fy.Regular27EQDeductee;
+import domain.in.rjsa.model.fy.Regular27QDeductee;
 import domain.in.rjsa.model.fy.Remark;
+import domain.in.rjsa.model.fy.Ticket;
 import domain.in.rjsa.service.AbstractServiceFY;
 import domain.in.rjsa.service.RemarkService;
 
@@ -22,6 +31,17 @@ public class RemarkServiceImpl extends AbstractServiceFY<Long, Remark,RemarkDao>
 	
 	@Autowired
 	RemarkDao dao;
+	@Autowired
+	Regular24QDeducteeDao r24qDao;
+	@Autowired
+	Regular26QDeducteeDao r26qDao;
+	@Autowired
+	Regular27EQDeducteeDao r27eqDao;
+	@Autowired
+	Regular27QDeducteeDao r27qDao;
+	@Autowired
+	TicketDao tDao;
+	
 	@Override
 	public RemarkDao getPrimaryDao() {
 		// TODO Auto-generated method stub
@@ -39,6 +59,60 @@ public class RemarkServiceImpl extends AbstractServiceFY<Long, Remark,RemarkDao>
 	public Class<Remark> getEntity() {
 		// TODO Auto-generated method stub
 		return Remark.class;
+	}
+
+	@Override
+	public void setResolve(LinkedHashMap<String, Object> entity, String type) {
+		int ID = (int) entity.get("deducteeId");
+		Long id = new Long(ID);
+		String remark = entity.get("deducteeForm").toString();
+		if (type.endsWith("Remark")) {
+			if (remark.endsWith("24Qform")) {
+				Regular24QDeductee r24q = r24qDao.getByKey(id);
+				r24q.setResolved(true);
+				r24qDao.update(r24q);
+			} else if (remark.endsWith("26Qform")) {
+				Regular26QDeductee r26q = r26qDao.getByKey(id);
+				r26q.setResolved(true);
+				r26qDao.update(r26q);
+			} else if (remark.endsWith("27EQform")) {
+				Regular27EQDeductee r27eq = r27eqDao.getByKey(id);
+				r27eq.setResolved(true);
+				r27eqDao.update(r27eq);
+			} else if (remark.endsWith("27Qform")) {
+				Regular27QDeductee r27q = r27qDao.getByKey(id);
+				r27q.setResolved(true);
+				r27qDao.update(r27q);
+			} else if (remark.endsWith("ticket")) {
+				Ticket ticket = tDao.getByKey(id);
+				ticket.setResolved(true);
+				tDao.update(ticket);
+			}
+		}else if(type.endsWith("Resolve")||type.endsWith("Reject"))
+		{	
+			if (remark.endsWith("24Qform")) {
+				Regular24QDeductee r24q = r24qDao.getByKey(id);
+				r24q.setResolved(false);
+				r24qDao.update(r24q);
+			} else if (remark.endsWith("26Qform")) {
+				Regular26QDeductee r26q = r26qDao.getByKey(id);
+				r26q.setResolved(false);
+				r26qDao.update(r26q);
+			} else if (remark.endsWith("27EQform")) {
+				Regular27EQDeductee r27eq = r27eqDao.getByKey(id);
+				r27eq.setResolved(false);
+				r27eqDao.update(r27eq);
+			} else if (remark.endsWith("27Qform")) {
+				Regular27QDeductee r27q = r27qDao.getByKey(id);
+				r27q.setResolved(false);
+				r27qDao.update(r27q);
+			} else if (remark.endsWith("ticket")) {
+				Ticket ticket = tDao.getByKey(id);
+				ticket.setResolved(false);
+				tDao.update(ticket);
+			}
+		}
+		
 	}
 	
 }

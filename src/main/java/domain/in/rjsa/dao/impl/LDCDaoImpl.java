525266878b4c005ee3f75664eee0e0fe4c39f046
STAFF19112020-01 <STAFF19112020-01@DESKTOP-FH2RN8H>
2022-05-31 18:19:47 +0530

    bugs fixed

--- src/main/java/domain/in/rjsa/dao/impl/LDCDaoImpl.java
+++ src/main/java/domain/in/rjsa/dao/impl/LDCDaoImpl.java
@@ -93,6 +93,39 @@ public List<LDC> searchExcel(HashMap entity) {
      {
 	criteria.add(Restrictions.eqOrIsNull("PAN", entity.get("PAN")));
      }
+	 if(entity.get("SECTION_CODE")!=null)
+     {
+	criteria.add(Restrictions.eqOrIsNull("SECTION_CODE", entity.get("SECTION_CODE")));
+     }
+	 
+	 if(entity.get("LDC_NUMBER")!=null)
+     {
+	criteria.add(Restrictions.eqOrIsNull("LDC_NUMBER", entity.get("LDC_NUMBER")));
+     }
+	 if(entity.get("TAN")!=null)
+     {
+	criteria.add(Restrictions.eqOrIsNull("TAN", entity.get("TAN")));
+     }
+	 if(entity.get("FY")!=null)
+     {
+	criteria.add(Restrictions.eqOrIsNull("FY", entity.get("FY")));
+     }
+	 if(entity.get("LDC_RATE")!=null)
+     {
+	criteria.add(Restrictions.eqOrIsNull("LDC_RATE", entity.get("LDC_RATE")));
+     }
+	 if(entity.get("CERTIFICATE_LIMIT")!=null)
+     {
+	criteria.add(Restrictions.eqOrIsNull("CERTIFICATE_LIMIT", entity.get("CERTIFICATE_LIMIT")));
+     }
+	 if(entity.get("AMOUNT_CONSUMED")!=null)
+     {
+	criteria.add(Restrictions.eqOrIsNull("AMOUNT_CONSUMED", entity.get("AMOUNT_CONSUMED")));
+     }
+	 if(entity.get("NATURE_OF_PAYMENT")!=null)
+     {
+	criteria.add(Restrictions.eqOrIsNull("NATURE_OF_PAYMENT", entity.get("NATURE_OF_PAYMENT")));
+     }
 	
 	
 	return (List<LDC>) criteria.list();

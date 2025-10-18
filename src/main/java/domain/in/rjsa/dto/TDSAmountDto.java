80198c4eaffc257adbf6dc4abf1a4d7ad8970416
Abhishek Sawant <abhisheksawant@gmail.com>
2025-10-18 12:35:00 +0530

    TDSAmountDTo

---
+++ src/main/java/domain/in/rjsa/dto/TDSAmountDto.java
@@ -0,0 +1,13 @@
+package domain.in.rjsa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TDSAmountDto {
+
+	private String tan;
+	private Double amount;
+
+}
